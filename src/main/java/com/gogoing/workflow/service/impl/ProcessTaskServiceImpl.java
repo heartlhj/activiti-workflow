929fd7a1d1efa3247a10fae1035ebc41f752f800
lihaijun <754491577@qq.com>
2020-05-26 22:13:57 +0800

    任务驳回

--- src/main/java/com/gogoing/workflow/service/impl/ProcessTaskServiceImpl.java
+++ src/main/java/com/gogoing/workflow/service/impl/ProcessTaskServiceImpl.java
@@ -13,12 +13,8 @@ import org.activiti.bpmn.model.BpmnModel;
 import org.activiti.bpmn.model.FlowElement;
 import org.activiti.bpmn.model.FlowNode;
 import org.activiti.bpmn.model.SequenceFlow;
-import org.activiti.engine.ManagementService;
-import org.activiti.engine.RepositoryService;
-import org.activiti.engine.RuntimeService;
-import org.activiti.engine.TaskService;
+import org.activiti.engine.*;
 import org.activiti.engine.impl.cmd.AbstractCustomSqlExecution;
-import org.activiti.engine.runtime.Execution;
 import org.activiti.engine.task.Task;
 import org.springframework.stereotype.Service;
 
@@ -40,7 +36,7 @@ public class ProcessTaskServiceImpl implements ProcessTaskService {
     private RepositoryService repositoryService;
 
     @Resource
-    private RuntimeService runtimeService;
+    private HistoryService historyService;
 
     @Resource
     private TaskService taskService;
@@ -135,74 +131,74 @@ public class ProcessTaskServiceImpl implements ProcessTaskService {
         if (userId == null || "".equals(userId)) {
             throw new ProcessException("操作用户ID不能为空");
         }
-        String statusDesc = "驳回";
-        String rejectComment = statusDesc + "" +processRejectParam.getRejectComment();
 
-        Task task = taskService.createTaskQuery().taskId(taskId).taskCandidateOrAssigned(userId).singleResult();
-        if (null == task) {
+        String rejectComment = processRejectParam.getRejectComment();
+
+        Task taskCurrent = taskService.createTaskQuery().taskId(taskId).taskCandidateOrAssigned(userId).singleResult();
+        if (null == taskCurrent) {
             throw new ProcessException("驳回失败，失败原因：未查到任务信息或者你没有权限驳回该任务");
         }
 
-        if (task.isSuspended()) {
+        if (taskCurrent.isSuspended()) {
             throw new ProcessException("驳回失败，失败原因：任务被冻结,无法执行驳回该任务");
         }
         //获取流程模型
-        BpmnModel bpmnModel = repositoryService.getBpmnModel(task.getProcessDefinitionId());
+        BpmnModel bpmnModel = repositoryService.getBpmnModel(taskCurrent.getProcessDefinitionId());
         if (null == bpmnModel) {
             throw new ProcessException("驳回失败，失败原因：未查到流程信息！");
         }
 
 
-        String processInstanceId = task.getProcessInstanceId();
+        String processInstanceId = taskCurrent.getProcessInstanceId();
         FlowElement targetFlowElement = null;
-        if(StringUtil.isNotEmpty(processRejectParam.getTargetNodeId())){
+        if (StringUtil.isNotEmpty(processRejectParam.getTargetNodeId())) {
             //找到目标节点元素
             targetFlowElement = bpmnModel.getMainProcess().getFlowElement(processRejectParam.getTargetNodeId());
-        }else{
+        } else {
             targetFlowElement = BpmnUtil.startEventNextTaskId(bpmnModel);
         }
-        //查找当前任务的元素ID
-        Execution execution = runtimeService.createExecutionQuery().executionId(task.getExecutionId()).singleResult();
-        String activityId = execution.getActivityId();
-        log.warn("当前任务所在流程元素ID:{}", activityId);
-        FlowNode currentFlowNode = (FlowNode) bpmnModel.getMainProcess().getFlowElement(activityId);
+        //当前待审批节点定义Id集合
+        List<Task> taskList = taskService.createTaskQuery().processInstanceId(processInstanceId).list();
 
-        //记录原活动方向
-        List<SequenceFlow> oriSequenceFlows = new ArrayList<>();
-        oriSequenceFlows.addAll(currentFlowNode.getOutgoingFlows());
+        if (CollectionUtil.isNotEmpty(taskList)) {
+            BpmnModel newBpmnModel = bpmnModel;
+            Map<String, List<SequenceFlow>> stringListMap = BpmnUtil.invokeSequenceFlows(newBpmnModel, taskList, targetFlowElement);
 
-        //清理活动方向
-        currentFlowNode.getOutgoingFlows().clear();
-
-        //重新绘制流程图，从当前节点到开始节点
-        List<SequenceFlow> newSequenceFlowList = new ArrayList<>();
-
-        //绘制连线，加入流程信息，并组装到流程图
-        SequenceFlow newSequenceFlow = new SequenceFlow();
-        newSequenceFlow.setId("newSequenceFlowId-" + System.currentTimeMillis());
-        newSequenceFlow.setSourceFlowElement(currentFlowNode);
-        newSequenceFlow.setTargetFlowElement(targetFlowElement);
-        newSequenceFlowList.add(newSequenceFlow);
-        currentFlowNode.setOutgoingFlows(newSequenceFlowList);
-
-        try {
-            taskService.addComment(task.getId(), processInstanceId, rejectComment);
-
-            //将处理人加入流程变量
-            Map<String, Object> currentVariables = new HashMap<>(1);
-            //设置节点状态
-            taskService.setVariablesLocal(task.getId(),currentVariables);
-            taskService.setAssignee(task.getId(),userId);
-            //完成任务
-            taskService.complete(task.getId(), currentVariables);
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new ProcessException("驳回失败，失败原因：完成任务时失败！");
-        } finally {
-            //恢复原方向
-            currentFlowNode.setOutgoingFlows(oriSequenceFlows);
+            for (Task task : taskList) {
+                //记录原活动方向
+                List<SequenceFlow> oriSequenceFlows = new ArrayList<>();
+                //当前节点
+                oriSequenceFlows.addAll(stringListMap.get(task.getTaskDefinitionKey()));
+                FlowNode currentFlowNode = (FlowNode) bpmnModel.getMainProcess().getFlowElement(task.getTaskDefinitionKey());
+                try {
+                    Map<String, Object> variables = new HashMap<>();
+                    //当前操作节点
+                    if(task.getId().equals(taskCurrent.getId())){
+                        //设置当前审批人为提交人
+                        taskService.setAssignee(task.getId(), userId);
+                        // 保存任务评价
+                        if (StringUtil.isNotEmpty(rejectComment)) {
+                            taskService.addComment(task.getId(), task.getProcessInstanceId(), rejectComment);
+                        }
+                        //设置节点状态
+                        taskService.setVariablesLocal(task.getId(), variables);
+                        //完成
+                        taskService.complete(task.getId());
+                    }else{
+                        //完成
+                        taskService.complete(task.getId());
+                        //删除任务
+                        historyService.deleteHistoricTaskInstance(task.getId());
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    throw new ProcessException("流程撤回异常，异常原因：" + e.getMessage());
+                } finally {
+                    //恢复原方向
+                    currentFlowNode.setOutgoingFlows(oriSequenceFlows);
+                }
+            }
         }
-
         return Boolean.TRUE;
     }
 
